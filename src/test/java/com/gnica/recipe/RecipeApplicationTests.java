cab4b4bfa8cc6d1baf3df37b228121f1f52da51e
Gabriel Nica <gabriel.nica@sytac.io>
2023-05-19 15:31:48 +0200

    Initialize repository. Add basic dependencies

---
+++ src/test/java/com/gnica/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnica.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
